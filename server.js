2b708ffc76967f69facf940b7576748ab2751f1a
Ben Pirt <ben@pirt.co.uk>
2019-03-01 15:40:47 +0000

    Add functionality for calibration

--- server.js
+++ server.js
@@ -3,13 +3,18 @@
 var Server  = require('ws').Server,
     s       = new Server({port: 8899}),
     Mirobot = require('./lib/mirobot.js').Mirobot,
-    m       = new Mirobot();
+    m       = new Mirobot(),
+    http    = require('http');
 
 s.on('connection', function(ws) {
   console.log("New Connection");
   
   var respond = function(msg){
-    ws.send(JSON.stringify(msg));
+    try{
+      ws.send(JSON.stringify(msg));
+    }catch(err){
+      console.dir("Connection closed");
+    }
   }
   
   ws.on('message', function(data, flags) {
@@ -17,3 +22,35 @@ s.on('connection', function(ws) {
     m.handle_msg(msg, respond);
   });
 });
+
+// send request to the discovery server every 30 minutes
+var sendDiscovery = function(name){
+  name = name || 'Mirobot-sim';
+  console.log("Sending discovery request for " + name);
+  var post_options = {
+    host: 'local.mirobot.io',
+    port: '80',
+    path: '/?address=localhost&name=' + name,
+    method: 'POST'
+  };
+
+  var post_req = http.request(post_options, function(res) {
+    res.on('data', function () {});
+  });
+
+  post_req.on('error', function(e){
+    console.log("Error sending discovery request");
+  });
+
+  post_req.end();
+}
+
+sendDiscovery();
+setInterval(sendDiscovery, 1800000);
+/*
+for(var i = 0; i< 5; i++){
+  for(var j = 0; j< 10; j++){
+    sendDiscovery('Room%20' + i + '%3AMirobot-' + j);
+  }
+}
+*/
